337bb070e2f5a6d71fd02efb7b642ac47807165d
ypj <ypj2005@126.com>
2017-09-20 19:09:38 +0800

    implement edit/update/destroy and paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,28 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update success!"
+    else
+      render :edit
+    end
+  end
+
+  def sestroy
+    @posts = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post delete!"
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
